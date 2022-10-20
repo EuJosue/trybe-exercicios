492d2f8d0facc6e9d42181eab6cbf09eb6a88c19
Josué Kirchner <josueantoniok@gmail.com>
2022-10-20 15:43:27 -0300

    fixacao feito

--- front-end/2.2/fixacao/exercise-image-component/src/App.js
+++ front-end/2.2/fixacao/exercise-image-component/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 function App() {
+  const url = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image source={ url } alternativeText="Cute cat staring"/>
     </div>
   );
 }
